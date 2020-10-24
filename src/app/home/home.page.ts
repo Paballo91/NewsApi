cab739600766b3444ea32448044836890e41de61
Paballo91 <62050149+Paballo91@users.noreply.github.com>
2020-10-24 15:31:36 +0200

    News

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {NewsFeedsService} from '../news-feeds.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  articles:any
+
+  constructor(private newsService:NewsFeedsService) {
+    this.loadNews()
+  }
+
+  loadNews(){
+    this.newsService.getNews("top-headlines?country=us").subscribe( news =>{
+      this.articles = news['articles'];
+      console.log(this.articles);
+    })
+  }
+}
